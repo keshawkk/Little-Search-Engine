0afd0aea59b07d293d66856f3b9fd3e35daa36e9
Monika <monikaa2725@gmail.com>
2020-12-13 12:47:28 +0530

    updated user and admin page

--- src/client/ui/AdminPage.jsx
+++ src/client/ui/AdminPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Nav } from 'react-bootstrap';
+import { Button, Container, Nav,Form,Row,Col,InputGroup,FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -22,17 +22,47 @@ const logoutClick = (e) => {
   };
 
   return (
+    <div class="bodybg">
+     <br />
     <Container>
-      <div className="title"> Search For User</div>
-      <div>Some text </div>
-      
-      <Nav.Link onClick={logoutClick}>
-          <Button variant="primary" type="submit">
+      <Row>
+       <Col xs md lg="4">
+        <Form>
+         <Form.Group>
+          <Form.File id="FormControlFile" label="Upload your file" />
+         </Form.Group>
+        </Form>
+       </Col>
+       <Col xs md lg="4"></Col>
+       <Col xs md lg="4">
+        <Nav.Link onClick={logoutClick}>
+          <Button className="float-right" variant="primary" type="submit">
             Logout 
           </Button>
-     </Nav.Link> 
-      
+        </Nav.Link> 
+       </Col>
+       
+      </Row>
+      <br />
+      <Row>
+       <Col xs md lg="3"></Col>
+       <Col xs md lg="5">
+        <h4><center>Type below!</center></h4>
+        <br/>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Type here"
+            aria-label="Type here"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Append>
+           <Button variant="primary">Search</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        </Col>
+      </Row>
     </Container>
+    </div>
   );
 }
 
--- src/client/ui/UserPage.jsx
+++ src/client/ui/UserPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, Nav } from 'react-bootstrap';
+import { Button, Container, Nav,Row,Col,InputGroup,FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -22,17 +22,35 @@ const logoutClick = (e) => {
   };
 
   return (
+    <div class="bodybg">
     <Container>
-      <div className="title"> Search For User</div>
-      <div>Some text </div>
-      
       <Nav.Link onClick={logoutClick}>
-          <Button variant="primary" type="submit">
+          <Button className="float-right" variant="primary" type="submit">
             Logout 
           </Button>
      </Nav.Link> 
-      
     </Container>
+    <br/>
+    <Container>
+      <Row>
+        <Col xs md lg="3"></Col>
+        <Col xs md lg="5">
+          <h4><center>Type below!</center></h4>
+          <br/>
+          <InputGroup className="mb-3">
+            <FormControl
+            placeholder="Type here"
+            aria-label="Type here"
+            aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button variant="primary">Search</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Col>
+      </Row>
+    </Container>
+    </div>
   );
 }
 
